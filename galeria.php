b695fb99bc9f37cb81a1be558b9a28277162a07a
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2025-10-20 19:15:16 -0300

    Elimino nina

--- galeria.php
+++ galeria.php
@@ -3,9 +3,8 @@
 
        <h1 class="title">Galería</h1>
        <div class="row gallery">
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/1.webp" title="Foods" class="gallery-image" data-gallery><img src="images/photos/1.webp" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/2.webp" title="Coffee" class="gallery-image" data-gallery><img src="images/photos/2.webp" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/nina.webp" title="Travel" class="gallery-image" data-gallery><img src="images/photos/nina.webp" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/1.webp" title="Fruits" class="gallery-image" data-gallery><img src="images/photos/1.webp" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/12c.webp" title="Travel" class="gallery-image" data-gallery><img src="images/photos/12c.webp" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/5.webp" title="Fruits" class="gallery-image" data-gallery><img src="images/photos/5.webp" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/11c.webp" title="Summer" class="gallery-image" data-gallery><img src="images/photos/11c.webp" class="img-responsive"></a></div>
@@ -13,7 +12,6 @@
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/8.jpg" title="Rooms" class="gallery-image" data-gallery><img src="images/photos/8.jpg" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/9.jpg" title="Big Room" class="gallery-image" data-gallery><img src="images/photos/9.jpg" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/11.jpg" title="Living Room" class="gallery-image" data-gallery><img src="images/photos/11.jpg" class="img-responsive"></a></div>
-              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/1.webp" title="Fruits" class="gallery-image" data-gallery><img src="images/photos/1.webp" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/10.webp" title="Travel" class="gallery-image" data-gallery><img src="images/photos/10.webp" class="img-responsive"></a></div>
               <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/13.webp" title="Paisaje" class="gallery-image" data-gallery><img src="images/photos/13.webp" class="img-responsive"></a></div>
        </div>
